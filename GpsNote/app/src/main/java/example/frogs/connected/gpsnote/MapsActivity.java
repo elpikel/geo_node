35e3fc47167b159d51a354f6b0059b15ca9f474d
Karol Stosik <Karol.Stosik@powel.com>
2017-05-16 13:08:09 +0200

    Remove no title bar, fix hardcoded screen sizes

--- GpsNote/app/src/main/java/example/frogs/connected/gpsnote/MapsActivity.java
+++ GpsNote/app/src/main/java/example/frogs/connected/gpsnote/MapsActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -177,7 +178,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
