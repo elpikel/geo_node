9debda418586cc6fb4b40116f87294436d506a05
stos <stos.poczta@gmail.com>
2017-05-27 22:07:23 +0200

    Increase java heap size to 1g, enable xaml compilation, add not used extended map class

--- TagLife/TagLife/Controls/ExtendedMap.cs
+++ TagLife/TagLife/Controls/ExtendedMap.cs
@@ -1,9 +1,48 @@
-﻿using Xamarin.Forms.Maps;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 
 namespace TagLife.Controls
 {
     public class ExtendedMap : Map
     {
-         
+        public readonly BindableProperty BindableProperty = BindableProperty.Create(
+            nameof(Pinss),
+            typeof(List<Pin>),
+            typeof(ExtendedMap),
+            null,
+            BindingMode.OneWay,
+            null,
+            (bindable, value, newValue) =>
+            {
+                ReplacePins(bindable, newValue);
+            });
+
+        public List<Pin> Pinss
+        {
+            get { return (List<Pin>)GetValue(BindableProperty); }
+            set { SetValue(BindableProperty, value); }
+        }
+
+        // yep, thats dirty. implement smart removing later
+        private static void ReplacePins(BindableObject bindable, object newValue)
+        {
+            var extendedMap = (ExtendedMap)bindable;
+
+            var numberOfPins = extendedMap.Pins.Count;
+
+            for (int i = numberOfPins - 1; i >= 0; i--)
+            {
+                extendedMap.Pins.RemoveAt(i);
+            }
+
+            var newPins = (List<Pin>)newValue;
+
+            foreach (var pin in newPins)
+            {
+                extendedMap.Pins.Add(pin);
+            }
+        }
     }
 }
